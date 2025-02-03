a3daf78fefab90972ff19fdc3d5d4001acb7be0c
HermesOliho <hermes.oliho.m@gmail.com>
2025-02-03 09:07:42 +0200

    Ajout de Firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,10 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({ projectId: "ngmradi-malakisi", appId: "1:512827716021:web:9e9e66779a96357288e9fc", storageBucket: "ngmradi-malakisi.firebasestorage.app", apiKey: "AIzaSyCXKtMR7Fioo4G9PpEUGP55xE4ZuD0IJvU", authDomain: "ngmradi-malakisi.firebaseapp.com", messagingSenderId: "512827716021" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
